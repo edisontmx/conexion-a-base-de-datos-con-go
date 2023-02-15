6abe014706aa429c6ba28e3010d992af31fe6ff9
edisontmx <edison.mc@gmail.com>
2023-02-14 19:52:28 -0600

    agregue una funcion insert para insertar registros

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"time"
 
@@ -17,6 +18,10 @@ func main() {
 		panic(err)
 	}
 
+	/* err = insert(ctx, db, "el hombre mas rico de babilonia", "George Samuel Clason", time.Time{2020/11/})
+	if err != nil {
+		panic(err)
+	} */
 	type libro struct {
 		id     int64
 		nombre string
@@ -53,7 +58,7 @@ func createCon() (*sql.DB, error) {
 
 func buscarLibros(ctx context.Context, db *sql.DB) error {
 	qry := `SELECT id, nombre, autor, fecha  FROM  libros  WHERE  id =?`
-	row := db.QueryRowContext(ctx, qry, 3)
+	row := db.QueryRowContext(ctx, qry, 4)
 
 	var id int64
 	var nombre string
@@ -65,5 +70,22 @@ func buscarLibros(ctx context.Context, db *sql.DB) error {
 		return err
 	}
 	log.Println("ROW:", id, nombre, autor, fecha)
+	return nil
+}
+
+func insert(ctx context.Context, db *sql.DB, nombre string, autor string, fecha time.Time) error {
+
+	qryAdd := `INSERT INTO libros( nombre, autor, fecha) values(?,?,?) `
+
+	result, err := db.ExecContext(ctx, qryAdd, nombre, autor, fecha)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	fmt.Println("INSERTED ID: ", id)
+
 	return nil
 }
